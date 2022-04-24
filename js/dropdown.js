625051450d6f940f10ad0278c1c354b822422dce
qw1324 <1553396053skwa@gmail.com>
2022-04-24 13:20:24 -0400

    优化了下拉菜单

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,38 +1,63 @@
+var card_850 = document.getElementsByClassName("card_850")
+var card_855 = document.getElementsByClassName("card_855")
+var card_8820 = document.getElementsByClassName("card_8820")
+var card_8850 = document.getElementsByClassName("card_8850")
+
+function get_850(show) {
+    for (var i = 0; i < card_850.length; i++) {
+        card_850[i].style.display = show
+    }
+}
+
+function get_855(show) {
+    for (var i = 0; i < card_855.length; i++) {
+        card_855[i].style.display = show
+    }
+}
+
+function get_8820(show) {
+    for (var i = 0; i < card_8820.length; i++) {
+        card_8820[i].style.display = show
+    }
+}
+
+function get_8850(show) {
+    for (var i = 0; i < card_8850.length; i++) {
+        card_8850[i].style.display = show
+    }
+}
+
 $(document).ready(function () {
     $("#checkType").on("change", function () {
         var ct = document.getElementById("checkType");
         var index = ct.selectedIndex;
         var sv = ct.options[index].value;
         if (sv == "all") {
-            $("#card_850").fadeIn("fast");
-            $("#card_855").fadeIn("fast");
-            $("#card_8820").fadeIn("fast");
-            $("#card_8820_1").fadeIn("fast");
-            $("#card_8850").fadeIn("fast");
+            get_850("flex");
+            get_855("flex");
+            get_8820("flex");
+            get_8850("flex");
+
         } else if (sv == "850") {
-            $("#card_850").fadeIn("fast");
-            $("#card_855").fadeOut("fast");
-            $("#card_8820").fadeOut("fast");
-            $("#card_8820_1").fadeOut("fast");
-            $("#card_8850").fadeOut("fast");
+            get_850("flex");
+            get_855("none");
+            get_8820("none");
+            get_8850("none");
         } else if (sv == "855") {
-            $("#card_850").fadeOut("fast");
-            $("#card_855").fadeIn("fast");
-            $("#card_8820").fadeOut("fast");
-            $("#card_8820_1").fadeOut("fast");
-            $("#card_8850").fadeOut("fast");
+            get_850("none");
+            get_855("flex");
+            get_8820("none");
+            get_8850("none");
         } else if (sv == "8820") {
-            $("#card_850").fadeOut("fast");
-            $("#card_855").fadeOut("fast");
-            $("#card_8820").fadeIn("fast");
-            $("#card_8820_1").fadeOut("fast");
-            $("#card_8850").fadeOut("fast");
+            get_850("none");
+            get_855("none");
+            get_8820("flex");
+            get_8850("none");
         } else if (sv == "8850") {
-            $("#card_850").fadeOut("fast");
-            $("#card_855").fadeOut("fast");
-            $("#card_8820").fadeOut("fast");
-            $("#card_8820_1").fadeOut("fast");
-            $("#card_8850").fadeIn("fast");
+            get_850("none");
+            get_855("none");
+            get_8820("none");
+            get_8850("flex");
         }
     })
 })
